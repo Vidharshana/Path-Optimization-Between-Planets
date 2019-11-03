6af8a7f8f0fa2a4706289e9c8eb0ada727a65f7f
raven <raven@bust-of-pallas>
2019-11-03 02:38:43 -0500

    minimizer changes

--- minimizer-static.py
+++ minimizer-static.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 
 # generate positions and thrusts
 init_pos = np.array([0,0])
-end_pos = np.array([3,0])
+end_pos = np.array([1,0])
 #planet_positions = [[1,0], [0.5, 0.9], [0.7, -0.5]]
 #planet_piositions = [[1,0], [0.5, 0.5], [0.5, -0.5]]
-#planet_positions = [[1,0], [0.5, 0.5]]
+planet_positions = [[1,0], [0.5, 0.5]]
 #planet_positions = [[1, 0], [2, 0], [3, 0]]
 #planet_masses = [3, 2, 1]
 #planet_positions = [[1,0]]
+planet_masses = [1, 10]
 
 # get thrusts in polar coordinates
 def get_thrusts(angle_range, mag_range):
@@ -28,13 +29,13 @@ def simulate(thrust, init_pos, planet_poses, end_time ):
     # convert thrust to cartesian
     vel = [thrust[1]*np.cos(thrust[0]),thrust[1]*np.sin(thrust[0])]
     t = 0
-    dt = 0.01
+    dt = 0.001
     trajectory = {}
     distances = {}
     def get_force_on_ship(pos):
         force = 0
         for i in range(len(planet_poses)):
-            force += -planet_masses[i]*(pos - planet_poses[i])/(np.linalg.norm(pos-end_pos)+0.0001)**3
+            force += -planet_masses[i]*(pos - planet_poses[i])/(np.linalg.norm(pos-planet_poses[i])+0.0001)**3
         return force
     while t < end_time:
         #print(pos, t)
@@ -56,11 +57,11 @@ for i in range(10):
     print("iteration", i)
     # convert thrusts to np array
     thrusts = np.array(thrusts)
-    plt.xlim(-0.5, 5)
-    plt.ylim(-2, 2)
+    plt.xlim(-0.5, 1.5)
+    plt.ylim(-1, 1)
     meta_trajectories = []
     for thrust in thrusts:
-        trajectory, how_close, at_what_time, at_what_thrust = simulate(thrust, init_pos, planet_positions, 10)
+        trajectory, how_close, at_what_time, at_what_thrust = simulate(thrust, init_pos, planet_positions, 1)
         xs, ys = zip(*list(trajectory.values()))
         plt.plot(xs, ys)
         print(how_close, at_what_time)
@@ -68,10 +69,10 @@ for i in range(10):
 
     plt.scatter(*zip(*planet_positions), s=20, c="blue")
     #plt.savefig("orbits-test-case/orbits"+str(i)+".png")
-    plt.savefig("orbits-weird-case/less-acc-orbits"+str(i)+".png")
+    #plt.savefig("orbits-weird-case/less-acc-orbits"+str(i)+".png")
     #plt.savefig("orbits-series3/orbits"+str(i)+".png")
     #plt.savefig("orbits-series2/orbits"+str(i)+".png")
-    #plt.savefig("orbits-series1/orbits"+str(i)+".png")
+    plt.savefig("new-orbits"+str(i)+".png")
     #plt.show()
     plt.cla()
 
